ae3f66dbe861dc5757d1e2560b32f75a40563e5b
Michael Bickel <mib@developium.net>
2015-09-14 18:01:13 +0100

    updated Test

--- test/Test.hx
+++ test/Test.hx
@@ -1,15 +1,29 @@
 
-import enet.Enet;
+import enet.ENet;
 
 class Test {
         
     static function main() {
     	
-    	if (Enet.initialize() != 0) {
-    		trace("An error occurred while initializing ENet");
+    	if (ENet.initialize() != 0) {
+    		trace("An error occurred while initializing ENet.");
     		return;
     	}
 
-    	Enet.deinitialize();
+
+    	var adr:ENetAddress = null;
+    	adr.host = ENet.ENET_HOST_ANY;
+    	adr.port = 1234;
+
+    	var server:cpp.Pointer<ENetHost> = ENet.host_create(cpp.Pointer.fromHandle(adr), 32, 2, 0, 0);
+    	if (server == null) {
+    		trace("An error occurred while trying to create an ENet server host.");
+    		ENet.deinitialize();
+    		return;
+    	}
+
+    	ENet.host_destroy(server);
+
+    	ENet.deinitialize();
     }
 }
